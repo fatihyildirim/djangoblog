a1b20ff8225eeb60d4052ef5fa46da196939843f
fatihyildirim <fatihyildirim.ch@gmail.com>
2016-03-15 18:56:05 +0200

    new template added and bug fixed

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from app.models import Post, Category, Tag
 
 
 def index(request):
-	posts = Post.objects.filter(status=True)
+	posts = Post.objects.filter(status=True).order_by('-created_at')
 	data = {'posts': posts}
 
 	return render(request, 'index.html', data)
@@ -12,12 +12,13 @@ def detail(request, slug):
 	post = Post.objects.get(status=True, slug=slug)
 	data = {'post': post}
 
-	return render(request, 'article.html', data)
+	return render(request, 'post.html', data)
 
 def category(request, slug):
 	category = Category.objects.get(slug=slug)
 	posts = Post.objects.filter(status=True, category=category)
 	data = {'posts': posts}
+	data['page'] = category 
 
 	return render(request, 'inner.html', data)
 
@@ -25,5 +26,6 @@ def tag(request, slug):
 	tag = Tag.objects.get(slug=slug)
 	posts = Post.objects.filter(status=True, tag=tag)
 	data = {'posts': posts}
+	data['page'] = tag
 
 	return render(request, 'inner.html', data)
