d977745094d90aecbdd62ff4db17c55dd06bb60d
fatihyildirim <fatihyildirim.ch@gmail.com>
2016-03-15 20:38:38 +0200

    paginator feature added

--- app/views.py
+++ app/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from app.models import Post, Category, Tag
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
 	posts = Post.objects.filter(status=True).order_by('-created_at')
+	paginator = Paginator(posts, 5)
+
+	page = request.GET.get('page')
+
+	try:
+		post_per_page = paginator.page(page)
+	except PageNotAnInteger:
+		post_per_page = paginator.page(1)
+	except EmptyPage:
+		post_per_page = paginator.page(paginator.num_pages)
+
 	data = {'posts': posts}
+	data['post_per_page'] = post_per_page
 
 	return render(request, 'index.html', data)
 
